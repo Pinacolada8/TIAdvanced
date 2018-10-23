7325290bbe01925a1c953c4b88ac19a00106f3dd
Tuwtuw <eduardoalvesvrb@hotmail.com>
2018-10-23 16:32:42 -0300

    Add files via upload

---
+++ src/Pessoas/Clientes.java
@@ -0,0 +1,9 @@
+package Pessoas;
+
+public class Clientes extends Pessoas{
+
+    public Clientes(String nome, String endereco, String telefone, String email, String usuario, String senha, String tipo) {
+        super(nome, endereco, telefone, email, usuario, senha, tipo);
+    }
+       
+}
