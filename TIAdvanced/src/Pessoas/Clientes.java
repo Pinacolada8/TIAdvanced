885266d582d846b39b68b0ad36d787f2141769f6
Pinacolada8 <mdcmatheusmdc@gmail.com>
2018-10-26 20:29:49 -0300

    REMOVI PASTA DE DOENTE

--- TIAdvanced/src/Pessoas/Clientes.java
+++
@@ -1,9 +0,0 @@
-package Pessoas;
-
-public class Clientes extends Pessoas{
-
-    public Clientes(String nome, String endereco, String telefone, String email, String usuario, String senha, String tipo) {
-        super(nome, endereco, telefone, email, usuario, senha, tipo);
-    }
-       
-}
