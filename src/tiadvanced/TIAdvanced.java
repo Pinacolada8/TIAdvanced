bdcca59fe240b86488022077af17581f95f692cc
Pinacolada8 <39754336+Pinacolada8@users.noreply.github.com>
2018-10-26 20:37:38 -0300

    fix001

--- src/tiadvanced/TIAdvanced.java
+++ src/tiadvanced/TIAdvanced.java
@@ -8,7 +8,7 @@ public class TIAdvanced {
     public static void main(String[] args) {
         Sistema system = new Sistema();
         FIleIO  data = new FIleIO(system);
-        system.Cadastro("Teste", "rua teste", "1234-5678", "teste@teste", "teste", "123456", "ADM");
+        data.load();    
         
         system.inicia();
         
